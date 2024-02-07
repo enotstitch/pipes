7e3549496dccdb6ffbce04cfb7d5ea23e40043b7
Konstantin <mogilevkostya@yandex.ru>
2024-02-07 21:46:31 +0700

    Правки в скрипте "checkPolicy"

--- assets/js/functions/checkPolicy.js
+++ assets/js/functions/checkPolicy.js
@@ -1,24 +1,28 @@
-const checkPolicy = (formSelector, buttonSelector = '.button') => {
-	const form = document.querySelector(formSelector);
+const checkPolicy = (sectionSelector, buttonSelector = '.button') => {
+	const section = document.querySelector(sectionSelector);
+	const form = section.querySelector('form');
 	const policyCheckbox = form.querySelector('.checkbox__real');
+	const policyFakeCheckbox = form.querySelector('.checkbox__fake');
 	const sendButton = form.querySelector(buttonSelector);
 	sendButton.setAttribute('disabled', '');
 
 	policyCheckbox.addEventListener('click', () => {
 		if (policyCheckbox.checked) {
 			sendButton.removeAttribute('disabled');
+			policyFakeCheckbox.classList.add('checkbox__fake--active');
 		} else {
 			sendButton.setAttribute('disabled', '');
+			policyFakeCheckbox.classList.remove('checkbox__fake--active');
 		}
 	});
 };
 
 try {
-	checkPolicy('.application__form');
+	checkPolicy('.application');
 } catch {}
 try {
-	checkPolicy('.calc__form', '.calc-data__button');
+	checkPolicy('.calc', '.calc-data__button');
 } catch {}
 try {
-	checkPolicy('.consultation__form');
+	checkPolicy('.consultation');
 } catch {}
